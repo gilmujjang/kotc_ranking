19ff2043c22b055e08acb3e9842f4b3315f1e583
gilmujjang <alsanrlf@naver.com>
2021-03-17 18:10:01 +0900

    [ADD] 승,패 유저 등록기능 만들다맘

--- src/component/admin/regiMatch.js
+++ src/component/admin/regiMatch.js
@@ -16,11 +16,27 @@ const CreateUser = () => {
 
   useEffect(() => {
     dbService.collection("user").onSnapshot(snapshot => {
+      const userArray = [];
       snapshot.docs.map(doc => (
-        console.log(doc.data())
+        userArray.push(doc.data().name)
       ))
+      setAllUsers(userArray);
     })
   }, [])
+
+  const winnerChange = e => {
+    if (e.key === 'Enter') {
+      if(winners.length>=2){
+        alert('2명이하만')
+        return
+      }
+      setWinners(winners.concat(searchWinner))
+      setSearchWinner('');
+      e.preventDefault();
+    } else {
+      setSearchWinner(e.target.value);
+    }
+  };
 
   const regiMatch = (
     <div className='userMaker'>
@@ -28,18 +44,33 @@ const CreateUser = () => {
         <div className="needMargin matchResult">
           <span>
             <div>승</div>
-            <Input type="text" name='win'/>
+            <Input type="text" name='win' value={searchWinner} onChange={winnerChange} onKeyPress={winnerChange}/>
+            <div className="users">
+              <div className="flexWrap">
+                {winners.map(i => (
+                  <span className="targetUser">{i}</span>
+                ))}
+              </div>
+            </div>
           </span>
           <span className="versus"> VS </span>
           <span>
             <div>패</div>
             <Input type="text" name='lose'/>
+            <div className="users">
+              <div className="flexWrap">
+                {losers.map(i => (
+                  <span className="targetUser">{i}</span>
+                ))}
+              </div>
+            </div>
           </span>
         </div>
         <div className="needMargin">
           <span className="needMargin">시합일</span>
           <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
         </div>
+        <div className="needMargin">{allUsers}</div>
         <Button className="needMargin">전송</Button>
       </Form>
     </div>
