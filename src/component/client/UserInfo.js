ff5a3c57f4f4e9e04bb209af15c367ef9579b674
gilmujjang <alsanrlf@naver.com>
2021-04-01 19:10:37 +0900

    [ADD] Nivo 차트를 넣었는데 검은차트가 나옴

--- src/component/client/UserInfo.js
+++ src/component/client/UserInfo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ResponsiveLine  } from '@nivo/line'
 
 const UserInfo = ({allUsers, allGame, contentMode}) => {
   
@@ -6,6 +7,117 @@ const UserInfo = ({allUsers, allGame, contentMode}) => {
   allUsersByTime.sort(function(a,b){
     return a.time > b.time ? -1 : a.time < b.time ? 1: 0;
   })
+
+  const data = [
+    {
+      "id": "japan",
+      "color": "hsl(314, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 99
+        },
+        {
+          "x": "helicopter",
+          "y": 142
+        },
+        {
+          "x": "boat",
+          "y": 88
+        },
+        {
+          "x": "train",
+          "y": 100
+        },
+        {
+          "x": "subway",
+          "y": 283
+        },
+        {
+          "x": "bus",
+          "y": 30
+        },
+        {
+          "x": "car",
+          "y": 264
+        },
+        {
+          "x": "moto",
+          "y": 290
+        },
+        {
+          "x": "bicycle",
+          "y": 238
+        },
+        {
+          "x": "horse",
+          "y": 166
+        },
+        {
+          "x": "skateboard",
+          "y": 264
+        },
+        {
+          "x": "others",
+          "y": 287
+        }
+      ]
+    },
+    {
+      "id": "france",
+      "color": "hsl(99, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 242
+        },
+        {
+          "x": "helicopter",
+          "y": 49
+        },
+        {
+          "x": "boat",
+          "y": 182
+        },
+        {
+          "x": "train",
+          "y": 0
+        },
+        {
+          "x": "subway",
+          "y": 285
+        },
+        {
+          "x": "bus",
+          "y": 218
+        },
+        {
+          "x": "car",
+          "y": 15
+        },
+        {
+          "x": "moto",
+          "y": 2
+        },
+        {
+          "x": "bicycle",
+          "y": 235
+        },
+        {
+          "x": "horse",
+          "y": 72
+        },
+        {
+          "x": "skateboard",
+          "y": 49
+        },
+        {
+          "x": "others",
+          "y": 184
+        }
+      ]
+    }
+  ]
 
     const userInfo = allUsersByTime.map((user,index) => (
         <div className="userInfo" key={index}>
@@ -35,7 +147,73 @@ const UserInfo = ({allUsers, allGame, contentMode}) => {
     ))
     return (
         <div className="usersInformation">
-          {userInfo}
+          <div className="chartSample">
+            <ResponsiveLine
+              data={data}
+              margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+              xScale={{ type: 'point' }}
+              yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
+              axisTop={null}
+              axisRight={null}
+              axisBottom={{
+                  orient: 'bottom',
+                  tickSize: 5,
+                  tickPadding: 5,
+                  tickRotation: 0,
+                  legend: 'transportation',
+                  legendOffset: 36,
+                  legendPosition: 'middle'
+              }}
+              axisLeft={{
+                  orient: 'left',
+                  tickSize: 5,
+                  tickPadding: 5,
+                  tickRotation: 0,
+                  legend: 'count',
+                  legendOffset: -40,
+                  legendPosition: 'middle'
+              }}
+              enableGridX={false}
+              lineWidth={3}
+              pointSize={10}
+              pointColor={{ theme: 'background' }}
+              pointBorderWidth={2}
+              pointBorderColor={{ from: 'serieColor' }}
+              pointLabelYOffset={-16}
+              isInteractive={false}
+              enableCrosshair={false}
+              legends={[
+                  {
+                      anchor: 'bottom-right',
+                      direction: 'column',
+                      justify: false,
+                      translateX: 107,
+                      translateY: -15,
+                      itemsSpacing: 2,
+                      itemDirection: 'left-to-right',
+                      itemWidth: 88,
+                      itemHeight: 20,
+                      itemOpacity: 0.75,
+                      symbolSize: 12,
+                      symbolShape: 'circle',
+                      symbolBorderColor: 'rgba(0, 0, 0, 0.5)',
+                      effects: [
+                          {
+                              on: 'hover',
+                              style: {
+                                  itemBackground: 'rgba(0, 0, 0, .03)',
+                                  itemOpacity: 1
+                              }
+                          }
+                      ]
+                  }
+              ]}
+            />
+          </div>
+          <div className="userList">
+            {userInfo}
+          </div>
+          
         </div>
     )
 };
