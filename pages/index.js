2a3f5736d6f98c90cf3d9374000c53551cde4a42
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2021-05-05 01:18:59 +0900

    21-05-05 구글 로그인 추가

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,34 @@
 import { useEffect, useState } from 'react'
 import { authService } from '../src/fbase'
+import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
 import Head from 'next/head'
 import Footer from '../src/index/component/Footer'
 import Header from '../src/index/component/Header'
 import Main from '../src/index/component/Main'
 
 const Home = () => {
-  const user = authService.currentUser
   const [init, setInit] = useState(false)
-  const [userObj, setUserObj] = useState({displayName: null})
+  const [userObj, setUserObj] = useState({})
 
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if(user) {
+        console.log('index의 유저 :', user);
+        console.log(user.displayName, user.uid, user.photoURL);
         setUserObj({
+          ...userObj,
           displayName: user.displayName,
           uid: user.uid,
-          photoUrl: user.photoURL
+          photoURL: user.photoURL
         })
+        console.log(userObj);
       } else {
-        authService.signInAnonymously()
-        .catch((error) => {
-          console.log(error.code)
-          console.log(error.message)
-        })
+        console.log('없음');
+        // authService.signInAnonymously()
+        // .catch((error) => {
+        //   console.log(error.code)
+        //   console.log(error.message)
+        // })
       }
       setInit(true)
     })
@@ -31,13 +36,26 @@ const Home = () => {
 
   return (
     <>
-      <Head>
-        <title>index.js</title>
-        <meta name="description" content="BBABBA의 홈입니다."></meta>
-      </Head>
-      <Header />
+    <Head>
+      <title>index.js</title>
+      <meta name="description" content="BBABBA의 홈입니다."></meta>
+    </Head>
+    {
+    init ?
+    <>
+      <Header userObj={userObj} />
       <Main />
       <Footer />
+    </>
+    :
+    <Segment className="loading">
+      <Dimmer active>
+        <Loader size="huge">Loading</Loader>
+      </Dimmer>
+
+      <Image src='/images/wireframe/short-paragraph.png' />
+    </Segment>
+    }
     </>
   )
 }
--- src/index/component/Header.js
+++ src/index/component/Header.js
@@ -1,15 +1,49 @@
+import { useEffect, useState } from 'react'
+import { authService, firebaseInstance } from '../../fbase'
 import classNames from 'classnames'
 import styles from '../css/Header.module.css'
 
-export default function Header() {
+export default function Header({ userObj }) {
+  const [isSignedIn, setIsSignedIn] = useState(false)
+
+  const onGoogleSignIn = async () => {
+    const  provider = new firebaseInstance.auth.GoogleAuthProvider();
+    await authService.signInWithPopup(provider);
+
+    setIsSignedIn(true)
+  };
+  
+  const onGoogleSignOut = () => {
+    authService.signOut().then(() => {
+      // sign-out successful.
+      setIsSignedIn(false)
+    }).catch((error) => {
+      // An error happended.
+      console.log(error.code)
+      console.log(error.message)
+    })
+  }
+
+  useEffect(() => {
+    if(Object.keys(userObj).length === 0) {setIsSignedIn(false)}
+    else if(Object.keys(userObj).length !== 0) {setIsSignedIn(true)}
+  }, [])
+
   return (
     <div className={styles.header}>
       <div className={classNames({["container__index"]: true, [styles.container__index__header]: true})}>
         <h1 className={styles.logo}>가즈아</h1>
+        {
+        isSignedIn ?
         <ul>
-          <li className="button__index">Sign in</li>
-          <li className="button__index">Sign up</li>
+          <li className={styles.name}>도킹 완료</li>
+          <li className="button__index" onClick={onGoogleSignOut}>Sign out</li>
         </ul>
+        :
+        <ul>
+          <li className="button__index" onClick={onGoogleSignIn}>Sign in with Google</li>
+        </ul>
+        }
       </div>
     </div>
   )
