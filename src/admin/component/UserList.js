b58ea517225b422d7aa107d29716e87678394b19
gilmujjang <alsanrlf@naver.com>
2021-05-05 17:27:06 +0900

    21-05-05 임시 커밋

--- src/admin/component/UserList.js
+++ src/admin/component/UserList.js
@@ -6,13 +6,14 @@ import classNames from 'classnames';
 const UserList = ({allUsers}) => {
 
   const RecentUser = allUsers.map(user => (
-    <Toast>
-      <ToastHeader>
+    <div className={styles.toast}>
+      <div  className={styles.toastheader}>
         <div className={styles.spaceBetween}>
-          <div>{user.name}__</div>
-          <div>{user.status}</div>
+          <div className={styles.needMargin}>{user.name}</div>
+          _
+          <div className={styles.needMargin}>{user.status}</div>
         </div>
-      </ToastHeader>
+      </div>
       <div className={classNames({["needMargin"]: true, ["spaceBetween"]: true})}>
         <div>학번 : {user.studentid}</div>
         <div>학과 : {user.department}</div>
@@ -21,7 +22,7 @@ const UserList = ({allUsers}) => {
         <div>Rating : {user.rating}</div>
         <div>전적 : {user.game_win}승 {user.game_lose}패</div>
       </div>
-    </Toast>
+    </div>
   ))
 
   return (
