ccc2b9b669a70b3f8bdc5876d6c912748ddaf9d5
gilmujjang <alsanrlf@naver.com>
2021-05-05 16:58:50 +0900

    [ADD] createuser css 작업

--- src/admin/component/CreateUser.js
+++ src/admin/component/CreateUser.js
@@ -122,46 +122,44 @@ const CreateUser = () => {
   
   const userMaker = (
     <div className={styles.userMaker}>
-      <Form className={styles.noteWriter}>
-        <div className={styles.userProfileImage}>
-          <span>프사</span>
-          <Input type="file" name="file" id="exampleFile" onChange={onFileChange}/>
+      <div className={styles.userProfileImage}>
+        <span>프사</span>
+        <input type="file" name="file" id="exampleFile" onChange={onFileChange}/>
+      </div>
+      {attachment && (
+      <div className={styles.factoryForm__attachment}>
+        <img className={styles.profileImg} src={attachment} alt="profile image"/>
+        <div className={styles.factoryForm__clear} onClick={onClearAttachment}>
+          <span>Remove</span>
         </div>
-        {attachment && (
-        <div className={styles.factoryForm__attachment}>
-          <img className={styles.profileImg} src={attachment} alt="profile image"/>
-          <div className={styles.factoryForm__clear} onClick={onClearAttachment}>
-            <span>Remove</span>
-          </div>
-        </div>
-      )}
-        <div className={styles.needMargin}>
-          <span>이름</span>
-          <Input type="text" name='name' onChange={handleChange} value={name}/>
-        </div>
-        <div className={styles.needMargin}>
-          <span>학번</span>
-          <Input type="text" name='studentid' onChange={handleChange} value={studentid}/>
-        </div>
-        <div className={styles.needMargin}>
-          <span>학과</span>
-          <Input type="text" name='department' onChange={handleChange} value={department}/>
-        </div>
-        <div className={styles.needMargin}>
-          <span>초기레이팅</span>
-          <Input type="text" name='start_rating' onChange={handleChange} value={start_rating}/>
-        </div>
-        <div className={styles.needMargin}>
-          <span>상태</span>
-          <Input type="select" name='status' onChange={handleChange} value={status}>
-            <option>재학</option>
-            <option>졸업</option>
-            <option>휴학</option>
-            <option>외부인</option>
-          </Input>
-        </div>
-        <Button className={styles.needMargin} onClick={userMakeSubmit}>전송</Button>
-      </Form>
+      </div>
+    )}
+      <div className={styles.createuserlist}>
+        <span>이름</span>
+        <input className={styles.createuserinput} type="text" name='name' onChange={handleChange} value={name}/>
+      </div>
+      <div className={styles.createuserlist}>
+        <span>학번</span>
+        <input className={styles.createuserinput} type="text" name='studentid' onChange={handleChange} value={studentid}/>
+      </div>
+      <div className={styles.createuserlist}>
+        <span>학과</span>
+        <input className={styles.createuserinput} type="text" name='department' onChange={handleChange} value={department}/>
+      </div>
+      <div className={styles.createuserlist}>
+        <span>초기레이팅</span>
+        <input className={styles.createuserinput} type="text" name='start_rating' onChange={handleChange} value={start_rating}/>
+      </div>
+      <div className={styles.createuserlist}>
+        <span>상태</span>
+        <select className={styles.createuserinput} type="select" name='status' onChange={handleChange} value={status}>
+          <option>재학</option>
+          <option>졸업</option>
+          <option>휴학</option>
+          <option>외부인</option>
+        </select>
+      </div>
+      <button className={styles.createuserbtn} onClick={userMakeSubmit}>전송</button>
     </div>
   );
 
