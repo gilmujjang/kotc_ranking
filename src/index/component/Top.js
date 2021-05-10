6d5f4d6553d70c5fde68d8a2de7158e4b0f23f49
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2021-05-10 23:35:22 +0900

    05.10 Header.js를 Top.js로 변경, 정보 수정 위한 버튼과 Modal 추가

--- src/index/component/Top.js
+++ src/index/component/Top.js
@@ -2,11 +2,13 @@ import { useContext, useEffect, useState } from 'react'
 import { authService, firebaseInstance } from '../../fbase'
 import Link from 'next/link'
 import classNames from 'classnames'
-import styles from '../css/Header.module.css'
+import { Icon, Button, Header, Image, Modal } from 'semantic-ui-react'
+import styles from '../css/Top.module.css'
 import UserObjContext from '../../contextAPI/UserObjContext'
 
-export default function Header() {
+const Top = () => {
   const [isSignedIn, setIsSignedIn] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
   const [userObj, setUserObj] = useContext(UserObjContext)
 
   const onGoogleSignIn = async () => {
@@ -19,7 +21,9 @@ export default function Header() {
   const onGoogleSignOut = () => {
     authService.signOut().then(() => {
       // sign-out successful.
+      setUserObj({})
       setIsSignedIn(false)
+      console.log(userObj)
     }).catch((error) => {
       // An error happended.
       console.log(error.code)
@@ -27,19 +31,23 @@ export default function Header() {
     })
   }
 
+
   useEffect(() => {
     if(Object.keys(userObj).length === 0) {setIsSignedIn(false)}
     else if(Object.keys(userObj).length !== 0) {setIsSignedIn(true)}
-  }, [])
+  }, [userObj])
 
   return (
+    <>
     <div className={styles.header}>
       <div className={classNames({["container"]: true, [styles.container__index__header]: true})}>
         <h1 className={styles.logo}><Link href="/"><a>가즈아</a></Link></h1>
         {
         isSignedIn ?
         <ul>
+          <li><img src={userObj.photoURL} alt="user profile" /></li>
           <li className={styles.name}>{userObj.displayName} 님,</li>
+          <li><Icon name="setting" size="large" className={styles.icon__setting} onClick={() => {setIsModalOpen(true)}} /></li>
           <li className="button__index" onClick={onGoogleSignOut}>Sign out</li>
         </ul>
         :
@@ -49,5 +57,19 @@ export default function Header() {
         }
       </div>
     </div>
+    <Modal onClose={() => setIsModalOpen(false)} open={isModalOpen} size="small">
+      <Modal.Header>안녕하세요, {userObj.name}님?</Modal.Header>
+      <Modal.Description>
+        <Header>개인 정보 설정</Header>
+        <p>빠라 빠빠빠~</p>
+      </Modal.Description>
+      <Modal.Actions>
+        <Button>꺼져</Button>
+        <Button>드루와~</Button>
+      </Modal.Actions>
+    </Modal>
+    </>
   )
 }
+
+export default Top
