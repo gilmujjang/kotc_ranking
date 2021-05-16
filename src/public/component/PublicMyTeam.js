d4954bc3886dea849a3e8a285faadca2850f3d35
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2021-05-17 02:08:03 +0900

    05-17 내가 속한 그룹 카드 생성 완료

--- src/public/component/PublicMyTeam.js
+++ src/public/component/PublicMyTeam.js
@@ -1,4 +1,4 @@
-import { memo, useContext, useState } from 'react'
+import { memo, useState, useEffect, useContext } from 'react'
 import classNames from 'classnames'
 import { dbService } from '../../fbase'
 import styles from '../css/PublicMyTeam.module.css'
@@ -7,19 +7,56 @@ import UserObjContext from '../../contextAPI/UserObjContext'
 const MyTeam = () => {
   const [userObj, setUserObj] = useContext(UserObjContext)
   const [myTeamList, setMyTeamList] = useState([])
-  dbService.collection('whole_users').doc(userObj.uid).collection('가입한 그룹').get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(doc.data());
+  const src = 'https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+
+  const myTeam = myTeamList.map((el, index) => 
+    <div className={styles.team} key={index}>
+      <div className={styles.team_image}>
+        <img src={src} alt="team profile" />
+      </div>
+      <span className={styles.border}></span>
+      <h1 className={styles.team_name}>{el.group_name}</h1>
+      <h2 className={styles.team_introduce}>{el.group_introduce}</h2>
+      <ul className={styles.team_info}>
+        <li className={styles.created_date}>
+          <span className={styles.info_name}>그룹 생성일</span>
+          <span className={styles.info_content}>{el.created_date}</span>
+        </li>
+        <li className={styles.joined_date}>
+          <span className={styles.info_name}>그룹 가입일</span>
+          <span className={styles.info_content}>{el.joined_date}</span>
+        </li>
+        <li className={styles.member_number}>
+          <span className={styles.info_name}>그룹 멤버 수</span>
+          <span className={styles.info_content}>{el.number_of_member}</span>
+        </li>
+      </ul>
+    </div>
+  )
+
+  useEffect(() => {
+    dbService.collection('whole_users').doc(userObj.uid).collection('가입한 그룹').get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        setMyTeamList(myTeamList => [...myTeamList, {
+          group_name: doc.data().group_name,
+          group_introduce: doc.data().group_introduce,
+          joined_date: doc.data().joined_date,
+          created_date: doc.data().created_date,
+          number_of_member: doc.data().number_of_member
+         }]
+        )
+      })
     })
-  })
-//   const teamCard = 
-//   console.log();
+  }, [userObj])
 
   return (
     <>
       <div className={styles.myTeam}>
         <div className={classNames({["container"]: true, [styles.container__myTeam]: true})}>
-          test
+          <h1>가입한 그룹</h1>
+          <div className={styles.teams}>
+            {myTeam}
+          </div>
         </div>
       </div>
     </>
