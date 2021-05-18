ed77e0f59bcdef561be05a88d9db30cb16d86374
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2021-05-18 15:22:22 +0900

    05-18 그룹 가입 신청과 이미 가입한 유저 필터 추가

--- pages/public/public_joinGroup.js
+++ pages/public/public_joinGroup.js
@@ -12,13 +12,31 @@ const public_joinGroup = () => {
   const [groupFilter, setGroupFilter] = useState('')
   const [mapGroupList, setMapGroupList] = useState([])
 
-  console.log('전체 : ', wholeGroups);
-  console.log('필터 : ', mapGroupList);
+  function requestJoin(e) {
+    const memberDocRef = dbService.collection(e.target.dataset.group_name).doc('group members').collection('멤버 목록').doc(userObj.uid)
+    const awaitorDocRef = dbService.collection(e.target.dataset.group_name).doc('group awaitors').collection('가입 대기자 목록').doc(userObj.uid)
+
+    memberDocRef.get().then((doc) => {
+      if(doc.exists) {
+        // 신청한 사람의 uid가 멤버 목록에 존재 할 경우
+        alert('이미 가입한 그룹입니다. :D')
+      } else {
+        // 신청한 사람의 uid가 멤버 목록에 존재 하지 않을 경우
+        awaitorDocRef.set({
+          name: userObj.name,
+          displayName: userObj.displayName,
+          photoURL: userObj.photoURL,
+          uid: userObj.uid
+        })
+      }
+    })
+  }
+
   const groupSmallCard = mapGroupList.map((el, index) => 
     <div className={styles.card} key={index}>
       <h1 className={styles.group_name}>{el.group_name}</h1>
       <h2 className={styles.group_introduce}>{el.group_introduce}</h2>
-      <div>가입 신청</div>
+      <div onClick={requestJoin} data-group_name={el.group_name}>가입 신청</div>
     </div>
   )
 
@@ -52,7 +70,7 @@ const public_joinGroup = () => {
       <div className={styles.joinGroup}>
         <div className={classNames({["container"]: true, [styles.container__public_joinGroup]: true})}>
           <input type="text" onChange={traceInputChange} value={groupFilter} placeholder="원하는 그룹명을 입력하세요." />
-          <div className={styles.groupSmallCardContainer}>
+          <div>
             {groupSmallCard}
           </div>
         </div>
