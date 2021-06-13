8015252a5271f1ea432937841c7b32a2c2a8246a
gilmujjang <alsanrlf@naver.com>
2021-06-13 16:10:41 +0900

    정리

--- .barbelrc.js
+++
@@ -1,5 +0,0 @@
-const env = require('./env-config')
-
-module.exports = {
-  presets: ['next/babel'],
-}
--- next.config.js
+++
@@ -1,9 +0,0 @@
-const debug = process.env.NODE_ENV !== 'production'
-const name = 'kotc_ranking'
-
-module.exports = {
-  future: {
-    webpack5: true,
-  },
-  target: "serverless"
-}
